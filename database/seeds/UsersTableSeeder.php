463ce14ae374b67bd25d311239cbfde9a29ea8a4
Pedro <pedroaccastilho@gmail.com>
2019-04-28 15:02:43 -0300

    First deploy
    first deploy with standard users seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      DB::table('users')->insert([
+          'name' => 'admin',
+          'email' => 'admin@gmail.com',
+          'isAdm' => true,
+          'password' => bcrypt('123')
+      ]);
+      DB::table('users')->insert([
+            'name' => 'user',
+            'email' => 'user@gmail.com',
+            'isAdm' => false,
+            'password' => bcrypt('123')
+        ]);
+    }
+}
