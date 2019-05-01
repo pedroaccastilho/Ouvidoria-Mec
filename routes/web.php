5ae332e14e3c980d9f9961b8497947257aa9ae70
Pedro <pedroaccastilho@gmail.com>
2019-05-01 16:39:48 -0300

    Rotas Welcome
    remocao de welcome padrao laravel

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,6 @@
 |
 */
 
-Route::get('/welcome', function () {
-    return view('welcome');
-});
-
 Route::get('/',['as'=>'login.index','uses'=>'LoginController@index']);
 Route::post('/login/entrar',['as'=>'login.entrar','uses'=>'LoginController@entrar']);
 
